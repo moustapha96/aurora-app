730ddb1a00a612175c0331a60cfa708596d6d4eb
Al hussein Khouma/Full Stack Developper <68778997+moustapha96@users.noreply.github.com>
2026-01-13 10:32:27 +0000

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -12,7 +12,7 @@ export default defineConfig(({ mode }) => ({
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "../app.aurorasociety.ch"),
+      "@": path.resolve(__dirname, "./build"),
     },
   },
 }));
